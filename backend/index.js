9ca30886d49a8847a30fa074fe1c12a4308e1825
harpreet-1 <hpsllg8@gmail.com>
2023-03-26 16:27:27 +0530

    done

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const { connection } = require("./server");
+
+const { usersRouter } = require("./Controller/routes/userRouter");
+const { todoRouter } = require("./Controller/routes/todoRouter");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/todos", todoRouter);
+app.use("/users", usersRouter);
+
+app.listen(8000, () => {
+  connection();
+});
